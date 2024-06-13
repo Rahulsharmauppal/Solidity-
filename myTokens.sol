f99c1ae863ec0d4fe8245e30f4046ff9ab0e43b8
Rahulsharmauppal <rahulaz408@gmail.com>
2024-06-13 12:02:47 +0530

    Create myTokens.sol

---
+++ myTokens.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.6.12 <0.9.0;
+
+// SPDX-License-Identifier: MIT
+
+contract MyToken {
+    // public variables here
+    string public tokenName = "META";
+    string public tokenAbbrv = "MTA";
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    // burn function
+    function burn(address _address, uint _value) public {
+        require(balances[_address] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_address] -= _value;
+    }
+}
